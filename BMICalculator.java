78c08f2746daf55e93aa75019eead289bb4dcbe2
Allen Rhodes <war2678@yahoo.com>
2022-02-17 20:32:49 -0600

    upload

---
+++ BMICalculator.java
@@ -0,0 +1,28 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class BMICalculator {
+    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter weight in pounds:");
+        double weight = input.nextDouble();
+
+        System.out.println("Enter height in feet:");
+        int heightFeet = input.nextInt();
+
+        System.out.println("Enter height in inches:");
+        int heightInches = input.nextInt();
+
+        int totalHeightInInches = (heightFeet * 12) + heightInches;
+        input.close();
+
+        double bmi = weight / Math.pow(totalHeightInInches, 2) * 703;
+
+        DecimalFormat df = new DecimalFormat("0.0");
+        System.out.println("Your BMI is : " + df.format(bmi));
+
+    }
+}
